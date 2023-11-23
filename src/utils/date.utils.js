a23881ff88927c20b6790c60fe0bee6f4dfb3d87
Albert Mercadé <albertmercaplas@gmail.com>
2023-11-23 22:04:36 +0100

    Updates

--- src/utils/date.utils.js
+++
@@ -1,9 +0,0 @@
-export const nextDrawDate = () => {
-  const currDate = new Date();
-  const currYear = currDate.getFullYear();
-  const lotteryDate = new Date(`${currYear}-12-22T09:00:00.000+01:00`);
-
-  if (lotteryDate - currDate < 0) lotteryDate.setFullYear(currYear + 1);
-
-  return lotteryDate;
-};
